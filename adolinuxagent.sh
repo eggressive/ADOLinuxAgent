d088bebf236f2876355895c6ba87c6bb0dbbb9bb
Dimitar <47664474+eggressive@users.noreply.github.com>
2022-11-03 18:26:59 +0100

    final tested version

--- adolinuxagent.sh
+++ adolinuxagent.sh
@@ -2,15 +2,15 @@
 # ADO Linux agent RHEL8
 # https://github.com/eggressive/ADOLinuxAgent.git
 
-dnf -y install wget git
+sudo dnf -y install wget
 wget https://vstsagentpackage.azureedge.net/agent/2.211.1/vsts-agent-linux-x64-2.211.1.tar.gz
 mkdir myagent && cd myagent
 tar zxvf ~/vsts-agent-linux-x64-2.211.1.tar.gz
 ./config.sh --unattended \
-	--url https://dev.azure.com/likeabossmitko \
-	--auth pat \
-	--token pnpm3mqumktbt7lteoaxx4knzmaxnvxhno7c6du3imcpjatt7k3a \
-	--pool RHELpool \
-	--agent rhelpool03
-./svc.sh install
-./svc.sh start
+        --url https://dev.azure.com/likeabossmitko \
+        --auth pat \
+        --token pnpm3mqumktbt7lteoaxx4knzmaxnvxhno7c6du3imcpjatt7k3a \
+        --pool RHELpool \
+        --agent rhelpool03
+sudo ./svc.sh install
+sudo ./svc.sh start
